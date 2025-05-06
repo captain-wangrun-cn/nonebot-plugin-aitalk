4058cc640423d795fec239d661736eb01f46bf9a
newcovid <280310454@qq.com>
2025-05-06 21:57:43 +0800

    新增分群不同提示词功能;其他中量代码优化

--- nonebot_plugin_aitalk/cd.py
+++ nonebot_plugin_aitalk/cd.py
@@ -3,10 +3,12 @@ from .config import chat_cd
 
 cd = {}
 
+
 def add_cd(uid: str):
     if uid not in cd:
         cd[uid] = 0
-    cd[uid] = time.time()+chat_cd
+    cd[uid] = time.time() + chat_cd
+
 
 def check_cd(uid: str):
     if uid not in cd:
